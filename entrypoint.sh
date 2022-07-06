5ca19ca574e4edf067bdcf0c51d9974301371d11
ff6347 <hash@ff6347.email>
2022-07-06 16:09:54 +0200

    style: Make entrypoint and env vars more readable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,32 @@ set -e
 IFS=$'\n\t'
 
 echo "system: Running $0"
-echo "system: Creating tileset directory...$TILESETDIR"
-mkdir -p "$TILESETDIR"
+echo "system: Creating tileset directory...$TILESET_DIR"
+mkdir -p "$TILESET_DIR"
+
 echo "system: Removing old geojson files..."
-rm -f "$WORKDIR"/out.full.geojson || true
+rm -f "$WORK_DIR"/out.full.geojson || true
+
 echo "ogr2ogr: Creating geojson file from sql query"
-ogr2ogr -f GeoJSON "$WORKDIR"/out.full.geojson "PG:host='$PGHOST' dbname='$PGDATABASE' user='$PGUSER' password='$PGPASSWORD'" -sql @"$WORKDIR"/ogr2ogr.sql
+ogr2ogr \
+  -f GeoJSON "$WORK_DIR"/out.full.geojson \
+  "PG:host='$POSTGRES_HOST' \
+  dbname='$POSTGRES_DB' \
+  user='$POSTGRES_USER' \
+  password='$POSTGRES_PASSWORD'" \
+  -sql @"$WORK_DIR"/ogr2ogr.sql
+
 echo "tippecanoe: Creating tileset..."
-tippecanoe -zg -o "$WORKDIR"/trees.mbtiles --force --drop-densest-as-needed --extend-zooms-if-still-dropping "$WORKDIR"/out.full.geojson
+tippecanoe \
+  -zg \
+  -o "$WORK_DIR"/trees.mbtiles \
+  --force \
+  --drop-densest-as-needed \
+  --extend-zooms-if-still-dropping \
+  "$WORK_DIR"/out.full.geojson
+
 echo "system: Copying tileset to tileset directory..."
-cp "$WORKDIR"/trees.mbtiles "$TILESETDIR"
+cp "$WORK_DIR"/trees.mbtiles "$TILESET_DIR"
+
 echo "mbtileserver: Starting tile server with args: $*"
 mbtileserver "$@"
