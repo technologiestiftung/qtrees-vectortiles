ec8ebaf9a124136a3551bb22b84662baa2f429a3
ff6347 <hash@ff6347.email>
2022-07-06 14:42:48 +0200

    feat: Move env variables to docker-compose file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,16 @@
 set -e
 IFS=$'\n\t'
 
-WORKDIR=/usr/app
-TILESETDIR=/tilesets
-
-echo "Running $0"
-rm -f $WORKDIR/out.full.geojson || true
-ogr2ogr -f GeoJSON $WORKDIR/out.full.geojson "PG:host='$PGHOST' dbname='$PGDATABASE' user='$PGUSER' password='$PGPASSWORD'" -sql @$WORKDIR/ogr2ogr.sql
-tippecanoe -zg -o $WORKDIR/trees.mbtiles --force --drop-densest-as-needed --extend-zooms-if-still-dropping $WORKDIR/out.full.geojson
-cp $WORKDIR/trees.mbtiles $TILESETDIR
-
-mbtileserver --dir $TILESETDIR --enable-fs-watch "$@"
+echo "system: Running $0"
+echo "system: Creating tileset directory...$TILESETDIR"
+mkdir -p "$TILESETDIR"
+echo "system: Removing old geojson files..."
+rm -f "$WORKDIR"/out.full.geojson || true
+echo "ogr2ogr: Creating geojson file from sql query"
+ogr2ogr -f GeoJSON "$WORKDIR"/out.full.geojson "PG:host='$PGHOST' dbname='$PGDATABASE' user='$PGUSER' password='$PGPASSWORD'" -sql @"$WORKDIR"/ogr2ogr.sql
+echo "tippecanoe: Creating tileset..."
+tippecanoe -zg -o "$WORKDIR"/trees.mbtiles --force --drop-densest-as-needed --extend-zooms-if-still-dropping "$WORKDIR"/out.full.geojson
+echo "system: Copying tileset to tileset directory..."
+cp "$WORKDIR"/trees.mbtiles "$TILESETDIR"
+echo "mbtileserver: Starting tile server with args: $*"
+mbtileserver "$@"
