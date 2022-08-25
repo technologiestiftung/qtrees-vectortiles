b0e0c084dc59dca5b36bb369ed5e66ce8dd1b86f
ff6347 <hash@ff6347.email>
2022-08-25 12:15:03 +0200

    ci(nightly): Add workflow for nightly tasks skip ci

--- release.config.js
+++ release.config.js
@@ -1,14 +1,20 @@
 module.exports = {
-  branches: [{ name: "main" }],
-  npmPublish: false,
-  dryRun: false,
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/npm",
-    "@semantic-release/git",
-    "@semantic-release/github",
-    "@semantic-release/release-notes-generator",
-  ],
+	branches: [{ name: "main" }],
+	npmPublish: false,
+	dryRun: false,
+	plugins: [
+		[
+			"@semantic-release/commit-analyzer",
+			{
+				preset: "angular",
+				releaseRules: [{ type: "nightly", release: "patch" }],
+			},
+		],
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		"@semantic-release/npm",
+		"@semantic-release/git",
+		"@semantic-release/github",
+		"@semantic-release/release-notes-generator",
+	],
 };
