2a4513eca775b7ef9db908e3c88459527b3b4980
ff6347 <hash@ff6347.email>
2022-12-08 11:48:36 +0100

    chore: Rename entrypoint to generator
    removes server
    makes github action from generator to create tiles on ci

---
+++ generator.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+IFS=$'\n\t'
+
+echo "system: Running $0"
+echo "system: Creating tileset directory...$INPUT_TILESET_DIR"
+mkdir -p "$INPUT_TILESET_DIR"
+mkdir -p "$INPUT_TMP_DIR"
+
+echo "system: Removing old geojson files..."
+rm -f "$INPUT_TMP_DIR"/out.full.geojson || true
+
+echo "ogr2ogr: Creating geojson file from sql query"
+ogr2ogr \
+  -f GeoJSON "$INPUT_TMP_DIR"/out.full.geojson \
+  "PG:host='$INPUT_POSTGRES_HOST' \
+  dbname='$INPUT_POSTGRES_DB' \
+  user='$INPUT_POSTGRES_USER' \
+  password='$INPUT_POSTGRES_PASSWORD'" \
+  -sql @"$INPUT_WORK_DIR"/ogr2ogr.sql
+
+echo "tippecanoe: Creating tileset..."
+tippecanoe \
+  -zg \
+  -o "$INPUT_TMP_DIR/$INPUT_TILESET_NAME" \
+  --force \
+  --drop-densest-as-needed \
+  --extend-zooms-if-still-dropping \
+  "$INPUT_TMP_DIR"/out.full.geojson
+
+echo "system: Copying tileset to workdir directory..."
+cp "$INPUT_TMP_DIR"/"$INPUT_TILESET_NAME" "$INPUT_WORK_DIR"
+echo "tileset_path=$INPUT_WORK_DIR/$INPUT_TILESET_NAME" >>"$GITHUB_OUTPUT"
