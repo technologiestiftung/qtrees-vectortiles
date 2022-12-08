a31aca2a5b3569743e2e5f0a6cd7deeb502d1312
ff6347 <hash@ff6347.email>
2022-12-08 13:35:06 +0100

    update generator [skip ci]

--- generator.sh
+++ generator.sh
@@ -3,31 +3,31 @@ set -e
 IFS=$'\n\t'
 
 echo "system: Running $0"
-echo "system: Creating tileset directory...$INPUT_TILESET_DIR"
-mkdir -p "$INPUT_TILESET_DIR"
-mkdir -p "$INPUT_TMP_DIR"
+echo "system: Creating tileset directory...$TILESET_DIR"
+mkdir -p "$TILESET_DIR"
+mkdir -p "$TMP_DIR"
 
 echo "system: Removing old geojson files..."
-rm -f "$INPUT_TMP_DIR"/out.full.geojson || true
+rm -f "$TMP_DIR"/out.full.geojson || true
 
 echo "ogr2ogr: Creating geojson file from sql query"
 ogr2ogr \
-  -f GeoJSON "$INPUT_TMP_DIR"/out.full.geojson \
-  "PG:host='$INPUT_POSTGRES_HOST' \
-  dbname='$INPUT_POSTGRES_DB' \
-  user='$INPUT_POSTGRES_USER' \
-  password='$INPUT_POSTGRES_PASSWORD'" \
+  -f GeoJSON "$TMP_DIR"/out.full.geojson \
+  "PG:host='$POSTGRES_HOST' \
+  dbname='$POSTGRES_DB' \
+  user='$POSTGRES_USER' \
+  password='$POSTGRES_PASSWORD'" \
   -sql @"$GITHUB_WORKSPACE"/ogr2ogr.sql
 
 echo "tippecanoe: Creating tileset..."
 tippecanoe \
   -zg \
-  -o "$INPUT_TMP_DIR/$INPUT_TILESET_NAME" \
+  -o "$TMP_DIR/$TILESET_NAME" \
   --force \
   --drop-densest-as-needed \
   --extend-zooms-if-still-dropping \
-  "$INPUT_TMP_DIR"/out.full.geojson
+  "$TMP_DIR"/out.full.geojson
 
 echo "system: Copying tileset to workdir directory..."
-cp "$INPUT_TMP_DIR"/"$INPUT_TILESET_NAME" "$GITHUB_WORKSPACE"
-echo "tileset_path=$GITHUB_WORKSPACE/$INPUT_TILESET_NAME" >>"$GITHUB_OUTPUT"
+cp "$TMP_DIR"/"$TILESET_NAME" "$GITHUB_WORKSPACE"
+echo "tileset_path=$GITHUB_WORKSPACE/$TILESET_NAME" >>"$GITHUB_OUTPUT"
