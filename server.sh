19db126d8ca4f6d882ff6b009eb2ebeac5d5f37a
ff6347 <hash@ff6347.email>
2022-12-08 16:41:08 +0100

    feat: Make server pull tileset from github

--- server.sh
+++ server.sh
@@ -6,10 +6,15 @@ echo "system: Running $0"
 echo "system: Creating tileset directory...$TILESET_DIR"
 mkdir -p "$TILESET_DIR"
 
-echo "system: downloading tileset to tileset directory... $TILESET_DIR"
-
 # download artifact from github actions and copy to $TILESET_DIR
-curl -L -o "$TILESET_DIR/$TILESET_NAME" "$TILESET_URL"
+echo "system: filtering build artifacts for tileset..."
+# save output of command in variable
+TILESET_URL=$(curl -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/technologiestiftung/qtrees-vectortiles-generator/actions/artifacts | jq '.artifacts[] | select(.name == "tileset") | .archive_download_url')
+echo "system: found the following url... $TILESET_URL"
+# download via curl from  $TILESET_URL and unzip to $TILESET_DIR
+echo "system: downloading tileset to tileset directory... $TILESET_DIR"
+curl -o trees-tileset.zip "$TILESET_URL"
+unzip trees-tileset.zip -d "$TILESET_DIR"
 
 echo "mbtileserver: Starting tile server with args: $*"
 mbtileserver "$@"
