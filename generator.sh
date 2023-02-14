ab6f8395c18ee82bbf67115de3408e5db5bc1365
ff6347 <hash@ff6347.email>
2023-02-14 13:46:01 +0100

    ci: Fix non existing base image

--- generator.sh
+++ generator.sh
@@ -12,17 +12,17 @@ rm -f "$TMP_DIR/$GEOJSON_OUTPUT_FILENAME" || true
 
 echo "$TMP_DIR"
 echo "$GEOJSON_OUTPUT_FILENAME"
-echo "$POSTGREST_API_URL"
+# echo "$POSTGREST_API_URL"
 echo "$POSTGRES_MATERIALIZE_VIEW_NAME"
-echo "ts-node: Creating geojson file from api request..."
-npx ts-node --esm "$GITHUB_WORKSPACE"/geojson-generator/index.ts
-# ogr2ogr \
-#   -f GeoJSON "$TMP_DIR"/out.full.geojson \
-#   "PG:host='$POSTGRES_HOST' \
-#   dbname='$POSTGRES_DB' \
-#   user='$POSTGRES_USER' \
-#   password='$POSTGRES_PASSWORD'" \
-#   -sql "@$GITHUB_WORKSPACE/ogr2ogr.sql"
+# echo "ts-node: Creating geojson file from api request..."
+# npx ts-node --esm "$WORKDIR"/geojson-generator/index.ts
+ogr2ogr \
+  -f GeoJSON "$TMP_DIR/$GEOJSON_OUTPUT_FILENAME" \
+  "PG:host='$POSTGRES_HOST' \
+  dbname='$POSTGRES_DB' \
+  user='$POSTGRES_USER' \
+  password='$POSTGRES_PASSWORD'" \
+  -sql "select * from $POSTGRES_MATERIALIZE_VIEW_NAME"
 
 echo "tippecanoe: Creating tileset..."
 tippecanoe \
@@ -31,10 +31,10 @@ tippecanoe \
   --force \
   --drop-densest-as-needed \
   --extend-zooms-if-still-dropping \
-  "$TMP_DIR"/out.full.geojson
+  "$TMP_DIR/$GEOJSON_OUTPUT_FILENAME"
 
 echo "system: Copying tileset to workdir directory..."
-cp "$TMP_DIR"/"$TILESET_NAME" "$GITHUB_WORKSPACE"
-echo "tileset_path=$GITHUB_WORKSPACE/$TILESET_NAME" >>"$GITHUB_OUTPUT"
+cp "$TMP_DIR"/"$TILESET_NAME" "$WORKDIR"
+echo "tileset_path=$WORKDIR/$TILESET_NAME" >>"$GITHUB_OUTPUT"
 echo "system: Upload tileset to s3 bucket"
 aws s3api put-object --bucket "$AWS_BUCKET" --key "$TILESET_NAME" --body "$TMP_DIR/$TILESET_NAME"
